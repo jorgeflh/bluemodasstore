938de652cb688edf78f2f9202c40abea4b3fa4dd
Jorge Filho <jorgeflh@gmail.com>
2020-07-06 22:22:54 -0300

    Add controller for Product model

---
+++ BlueModasStore/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using BlueModasStore.Domain.Interfaces.Services;
+using BlueModasStore.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueModasStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Product>> GetById(int id)
+        {
+            return await productService.GetProductById(id);
+        }
+    }
+}
---
+++ SQLScripts.sql
@@ -0,0 +1,17 @@
+﻿-- Create Product Database
+GO
+CREATE TABLE Product (
+    [Id] int IDENTITY(1,1) PRIMARY KEY,
+    [Name] VARCHAR(60) NOT NULL,
+    [Description] VARCHAR(255) NOT NULL,
+    [Price] DECIMAL(12,2) NOT NULL,
+    [Image] IMAGE
+);
+GO
+
+INSERT INTO Product (Name, Description, Price) 
+VALUES (
+    'Camiseta Homem de Ferro', 
+    'Camiseta da coleção Avengers 100% Algodão',
+    39.90
+    );
