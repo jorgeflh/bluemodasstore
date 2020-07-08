c073e04368bbd89771dac3000e85738fce031beb
Jorge Filho <jorgeflh@gmail.com>
2020-07-08 16:10:07 -0300

    Add customer service

---
+++ BlueModasStore.Domain/Interfaces/Services/ICustomerService.cs
@@ -0,0 +1,15 @@
+﻿using BlueModasStore.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlueModasStore.Domain.Interfaces.Services
+{
+    public interface ICustomerService
+    {
+        Task<List<Customer>> GetCustomers();
+        Task<Customer> GetCustomerById(int id);
+        Task<bool> AddCustomer(Customer customer);
+        Task<bool> UpdateCustomer(Customer customer);
+        Task<bool> RemoveCustomer(int id);
+    }
+}
---
+++ BlueModasStore.Domain/Services/CustomerService.cs
@@ -0,0 +1,43 @@
+﻿using BlueModasStore.Domain.Interfaces.Repositories;
+using BlueModasStore.Domain.Interfaces.Services;
+using BlueModasStore.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlueModasStore.Domain.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerRepository customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            this.customerRepository = customerRepository;
+        }
+
+        public Task<bool> AddCustomer(Customer customer)
+        {
+            return customerRepository.AddCustomer(customer);
+        }
+
+        public Task<Customer> GetCustomerById(int id)
+        {
+            return customerRepository.GetCustomerById(id);
+        }
+
+        public Task<List<Customer>> GetCustomers()
+        {
+            return customerRepository.GetCustomers();
+        }
+
+        public Task<bool> RemoveCustomer(int id)
+        {
+            return customerRepository.RemoveCustomer(id);
+        }
+
+        public Task<bool> UpdateCustomer(Customer customer)
+        {
+            return customerRepository.UpdateCustomer(customer);
+        }
+    }
+}
