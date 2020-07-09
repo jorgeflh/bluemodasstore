7ec15de55bb27e7ce6c66e606eaea22134dbde0d
Jorge Filho <jorgeflh@gmail.com>
2020-07-09 16:45:10 -0300

    Update image model

--- BlueModasStore.Domain/Models/Product.cs
+++ BlueModasStore.Domain/Models/Product.cs
@@ -7,6 +7,6 @@ namespace BlueModasStore.Domain.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
-        public Byte[] Image { get; set; }
+        public string Image { get; set; }
     }
 }
